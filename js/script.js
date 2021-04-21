b45852a63aa6d720690b8b328dab99fd18d5b9b2
MattiaManfreda <mattiamanfreda926@gmail.com>
2021-04-21 16:14:57 +0200

    step

---
+++ js/script.js
@@ -0,0 +1,26 @@
+// stampare in pagina un item per ogni elemento contenuto in un array
+// - ogni item ha una "x" associata: cliccando su di essa, l'item viene rimosso dalla lista
+// - predisporre un input per aggiungere un nuovo item alla lista: 
+// digitando il tasto invio oppure ciccando su un pulsante, 
+// il testo digitato viene aggiunto alla lista
+
+
+
+var app = new Vue({
+    el: '#root',
+    data: {
+
+        message: "nabbi",
+        items: [
+            
+        ]
+
+    },
+
+    methods: {
+        aggiungiLista() {
+            this.items.push(this.message);
+            console.log(this.items);
+        }
+    }
+})
